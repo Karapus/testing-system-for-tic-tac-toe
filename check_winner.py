72076b87f2ff7b22cd4a3743170a24a7df6a50f9
unknown <vsevolod-pl@yandex.ru>
2019-03-13 19:52:11 +0300

    Created dummy script for checking winner

---
+++ check_winner.py
@@ -0,0 +1,10 @@
+from random import randint
+
+n,m,l = map(int,input().split())
+types = int(input())
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int,input().split())))
+
+#for i in range()
+print(randint(0,1)*randint(0,types)-1)
