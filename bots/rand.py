7526cb8c0cdd6037196a4adacdb4c0b82ac9246e
Abrikos <abrikos1100@gmail.com>
2019-03-13 17:50:27 +0300

    Changing random bot

--- bots/rand.py
+++ bots/rand.py
@@ -1,15 +1,20 @@
 import random
 
-n_lines, n_columns = map(int, input().split())
-n_types = int(input())
-player_idx = int(input())
-field = [list(map(int, input().split())) for i in range(n_lines)]
+def play(n_lines, n_columns, n_types, player_idx, field):
+    free = []
+    for i in range(n_lines):
+        for j in range(n_columns):
+            if field[i][j] == -1:
+                free.append((i, j))
 
-free = []
-for i in range(n_lines):
-    for j in range(n_columns):
-        if field[i][j] == -1:
-            free.append((i, j))
+    return random.choice(free)
 
-print(*random.choice(free))
+if __name__ == '__main__':
+    n_lines, n_columns = map(int, input().split())
+    n_types = int(input())
+    player_idx = int(input())
+    field = [list(map(int, input().split()))
+             for i in range(n_lines)]
+    print(*play(n_lines, n_columns, n_types, player_idx, field))
+
 
