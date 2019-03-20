18dd1c721d3c2a6a5086acfc0c7648188b785aa4
Karapus <evgeny.nay@outlook.com>
2019-03-20 19:51:17 +0300

    #3Complited alpha of engine

--- engine.sh
+++ engine.sh
@@ -26,39 +26,41 @@ print_map(){
 draw_check(){
 	for i in "${map[@]}"
        	do
-		if [ $i=-1 ]; then 
-			return 0
+		if [ $i = -1 ]; then 
+			echo 0
+			exit
 		fi
 	done
-	return 1
+	echo 1
 };
 
 nplayers=$3
 nplayers=${nplayers:-2}
 newline=$'\n' 
 
-#echo ${map[${turn[0]} * $m + ${turn[1]}]}
-while [ 1 ]; do
-	for ((i=0;i<nplayers;i++)) do
-		echo "Player: $i; ${turn[0]}, ${turn[1]}" >> log.txt
-		if [ draw_check == 1 ]; then
+rm log.txt
+
+while [ 1 ]; 
+do
+	for i in $(seq 0 $[$nplayers-1])
+	do
+		if [ $(draw_check) = 1 ]; then
 			echo "it's draw!"
 			print_map
 			exit
 		fi
 		turn=$(echo "$n $m$newline$nplayers$newline$i$newline$(print_map)" | python bots/rand.py)
-		if [ $(echo "$n $m$newline$nplayers$newline$(print_map)$newline$turn$newline" | python check_field.py) = $'False' ]; then
+		turn=(${turn})
+		echo "Player: $i; ${turn[0]}, ${turn[1]}" >> log.txt
+		if [ $(echo "$n $m$newline$nplayers$newline$(print_map)$newline${turn[@]}$newline" | python check_field.py) = $'False' ]; then
 			echo "the $1 lose"
 			print_map
 			exit	
 		fi
-		turn=(${turn})
-		#echo "${turn[0]}, ${turn[1]}" 
-		#map[${turn[0]} * $m + ${turn[1]}]=$i
-		#print_map
-		#Add winner check
+		echo "${turn[0]}, ${turn[1]}" 
+		map[${turn[0]} * $m + ${turn[1]}]=$i
+		print_map
 	done
 done
 
 #visualisation
-echo ${map[${turn[0]} * $m + ${turn[1]}]}
